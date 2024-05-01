feca7cf83d3662effbf32731a0a5d3e5d57bea15
Patricia Kemirembe <kemimirembe2patricia@gmail.com>
2024-05-01 03:19:08 -0700

    AirBNB clone V2

--- models/amenity.py
+++ models/amenity.py
@@ -3,12 +3,12 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """ class amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+    from models.place import place_amenity
     place_amenities = relationship("Place",
                                    secondary=place_amenity)
