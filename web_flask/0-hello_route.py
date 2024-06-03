8b541657f0f2ff56a62f1d98b6e1f0fa1244dcd9
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-06-03 19:11:49 +0400

    update changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,12 +4,11 @@ Start a flask application.
 """
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-
-
 def index():
 	"""Returns Hello HBNB"""
 	return 'Hello HBNB!'
